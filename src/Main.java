317720afe1b99ce230669c9daae070c05b0d2e70
irinaaan <irinanorka04@gmail.com>
2022-10-02 15:57:39 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setSurname("Shevchenko");
+        person.setName("Vladimir");
+        person.setAge(23);
+        person.printInfo();
+    }
+}
